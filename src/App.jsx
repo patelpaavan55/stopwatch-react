8fb921c2c057ba18641bf7e7475f8ad8ec27c4d0
Your Name <patel.paavan55@gmail.com>
2021-07-06 22:36:07 -0400

    feat: added styling, StartStop, reset func working

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,126 @@
-import React, { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { useState, useEffect, useLayoutEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "./fonts/MyFont-UltraLight.otf";
+import "./fonts/SanFranciscoDisplayRegular.ttf";
 
-function App() {
-  const [count, setCount] = useState(0)
+const START_TEXT = "Start";
+const STOP_TEXT = "Stop";
+const LAP_TEXT = "Lap";
+const RESET_TEXT = "Reset";
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+const START_COLOR = "green";
+const STOP_COLOR = "red";
+
+function getFormattedTime(totalTime) {
+  const totalTimeinSeconds = totalTime / 1000;
+  const minutes = Math.floor(totalTimeinSeconds / 60);
+  const seconds = Math.floor(totalTimeinSeconds % 60);
+  const centiseconds = Math.floor((totalTime % 1000) / 10);
+  // prettier-ignore
+  return `${minutes.toString(10).padStart(2, 0)}:${seconds.toString(10).padStart(2, 0)}.${centiseconds.toString(10).padStart(2, 0)}`
 }
 
-export default App
+function App() {
+  const [currentElapsedTime, setCurrentElapsedTime] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+  const [startTime, setStartTime] = useState(0);
+  const [stopTime, setStopTime] = useState(0);
+  const [overallTime, setOverallTime] = useState(0);
+  const [currentTime, setCurrentTime] = useState(0);
+  const [isLapDisabled, setIsLapDisabled] = useState(true);
+
+  function handleStartStop() {
+    if (!isRunning) {
+      setStartTime(Date.now());
+      console.log("START HANDLE" + startTime);
+      setIsRunning(true);
+    } else {
+      setStopTime(Date.now());
+      setIsRunning(false);
+    }
+  }
+
+  function handleLapReset() {
+    const lapResetState = isRunning || isLapDisabled ? LAP_TEXT : RESET_TEXT;
+    if (lapResetState === LAP_TEXT) {
+      console.log("THis is the Lap State........." + lapResetState);
+    } else if (lapResetState === RESET_TEXT) {
+      setIsLapDisabled(true);
+      setCurrentTime(0);
+      setStartTime(0);
+      setStopTime(0);
+      setCurrentElapsedTime(0);
+      setOverallTime(0);
+    }
+  }
+
+  useEffect(() => {
+    console.log("Stoptime: " + stopTime);
+  }, [stopTime]);
+  useEffect(() => {
+    console.log("Starttime: " + startTime);
+  }, [startTime]);
+  useEffect(() => {
+    console.log("Overalltime: " + overallTime);
+  }, [overallTime]);
+
+  useEffect(() => {
+    if (isRunning) {
+      setIsLapDisabled(false);
+    }
+  }, [isRunning]);
+
+  useEffect(() => {
+    let intervalId;
+    if (isRunning) {
+      function updateCurrentTime() {
+        setCurrentTime(Date.now());
+      }
+      intervalId = setInterval(updateCurrentTime, 10);
+    }
+
+    return () => clearInterval(intervalId);
+  }, [isRunning]);
+
+  useEffect(() => {
+    setCurrentElapsedTime(currentTime - startTime + overallTime);
+  }, [currentTime, startTime]);
+
+  useEffect(() => {
+    setOverallTime(
+      (previousOverallTime) => previousOverallTime + (stopTime - startTime)
+    );
+  }, [stopTime]);
+
+  return (
+    <div className='App'>
+      <div className='layout-container'>
+        <div className='card'>
+          <div className='timer-display'>
+            {getFormattedTime(currentElapsedTime)}
+          </div>
+          <div className='timer-control-buttons'>
+            <button
+              className='btn-lap-reset round-button gray'
+              disabled={isLapDisabled}
+              onClick={handleLapReset}
+            >
+              {isRunning || isLapDisabled ? LAP_TEXT : RESET_TEXT}
+            </button>
+            <button
+              className={`btn-start-stop round-button ${
+                isRunning ? STOP_COLOR : START_COLOR
+              }`}
+              onClick={handleStartStop}
+            >
+              {isRunning ? STOP_TEXT : START_TEXT}
+            </button>
+          </div>
+          <ul className='laps-container'></ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default App;
